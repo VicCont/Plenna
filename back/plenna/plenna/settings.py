3debcc9267025e6efc02d6533d2eff860d888279
VicCont <vic.contraerads@gmail.com>
2022-05-22 14:51:34 -0500

    carga static

--- back/plenna/plenna/settings.py
+++ back/plenna/plenna/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -74,15 +74,10 @@ WSGI_APPLICATION = 'plenna.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'plenna',
-        'USER': 'postgres',
-        'PASSWORD': 'n0m3l0',
-        'HOST': '3.229.90.61',
-        'PORT': '',
-        }
+
 }
 
 # Password validation
@@ -119,8 +114,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
